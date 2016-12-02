94204bdaad01299181f853526388c736ba0c529f
West Kraemer <WestKraemer@gmail.com>
2016-12-02 15:11:09 -0500

    adding doubles and integers with conversions

--- playground1.playground/Contents.swift
+++ playground1.playground/Contents.swift
@@ -13,9 +13,20 @@ print(otherSrt)
 
 
 var Name = "West"
+var Age = "31"
 
 print("Hello " + Name)
 
 let myInt = 8
 
 print(myInt + 100)
+
+print("My name is \(Name) and I am \(Age) years old.")
+
+// Set a double to 5.76 and an integer to 8.
+
+let d:Double = 5.76
+
+let e:Int = 8
+
+print("The product of \(d) and \(e) is \(d * Double(e))")
