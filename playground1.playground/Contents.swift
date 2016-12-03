8d67eb4dc248f43db41a6c4d1c447e8d7c8d0d73
West Kraemer <WestKraemer@gmail.com>
2016-12-02 19:23:40 -0500

    working with if/then statements

--- playground1.playground/Contents.swift
+++ playground1.playground/Contents.swift
@@ -2,53 +2,12 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let age = 14
 
-print("West")
-
-print("hello")
-
-let otherSrt = "Hi West"
-
-print(otherSrt)
-
-
-var Name = "West"
-var Age = "31"
-
-print("Hello " + Name)
-
-let myInt = 8
-
-print(myInt + 100)
-
-print("My name is \(Name) and I am \(Age) years old.")
-
-// Set a double to 5.76 and an integer to 8.
-
-let d:Double = 5.76
-
-let e:Int = 8
-
-print("The product of \(d) and \(e) is \(d * Double(e))")
-
-
-var array = [3.87, 7.1, 8.9]
-
-array.remove(at: 1)
-
-array.append(array[0] * array[1])
-
-// dictionaries
-
-var menu = [String: Decimal]()
-
-menu["Pizza"] = 10.99
-
-menu["Ice Cream"] = 4.99
-
-menu["Salad"] = 7.99
-
-print ("The total cost of my meal is \(menu["Pizza"]! + menu["Ice Cream"]!)")
-
+if age > 18 {
+    print ("You can play!")
+}
+else {
+    print ("You're too young")
+}
 
